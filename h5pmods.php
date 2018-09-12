9f954cf2256c15e6ff249d12e97f20d00ccd1b53
marbaque <mario.badilla@gmail.com>
2018-09-12 14:29:25 -0600

    Update h5pmods.php

--- h5pmods.php
+++ h5pmods.php
@@ -122,7 +122,7 @@ function h5pmods_alter_styles(&$styles, $libraries, $embed_type) {
   $styles[] = (object) array(
     // Path can be relative to wp-content/uploads/h5p or absolute.
     'path' => '../../../plugins/h5pmods/custom-h5p.css',
-    'version' => '?ver=1.5' // Cache buster
+    'version' => '?ver=1.6' // Cache buster
   );
 }
 add_action('h5p_alter_library_styles', 'h5pmods_alter_styles', 10, 3);
